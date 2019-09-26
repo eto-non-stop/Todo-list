7ccdd2f1bedf735ce6b3cde2d801f0a57c0bc9c7
Mariya Tokina <mariya.tokina@gmail.com>
2019-09-26 14:47:50 +0300

    Add state

--- src/components/TodoListItem/index.jsx
+++ src/components/TodoListItem/index.jsx
@@ -3,16 +3,33 @@ import React, { Component } from "react";
 import styles from "./styles.module.css";
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  };
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = styles.todo_list_item;
+
+    if (done) {
+      classNames += ` ${styles.done}`;
+    }
 
     const style = {
-      color: important ? "rgb(160, 204, 90)" : "black",
+      color: important ? "rgb(160, 204, 90)" : "rgb(66, 66, 66)",
       fontWeight: important ? "bold" : "normal"
     };
 
     return (
-      <li style={style} className={styles.todo_list_item}>
+      <li style={style} className={classNames} onClick={this.onLabelClick}>
         {label}
         <div className={styles.btn_group}>
           <button className={styles.btn}>
