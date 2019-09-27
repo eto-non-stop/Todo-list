354eb2f3084055eb05dfadb0f0bef6456f2c44c0
Mariya Tokina <mariya.tokina@gmail.com>
2019-09-27 16:10:16 +0300

    Add item creation

--- src/components/AddItem/index.jsx
+++ src/components/AddItem/index.jsx
@@ -2,21 +2,35 @@ import React, { Component } from "react";
 import styles from "./styles.module.css";
 
 export default class AddInput extends Component {
+  state = {
+    label: ""
+  };
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ""
+    });
+  };
+
   render() {
     return (
-      <div className={styles.add_input_form}>
+      <form className={styles.add_input_form} onSubmit={this.onSubmit}>
         <input
           type="text"
-          placeholder="enter here"
+          placeholder="What needs to be done"
           className={styles.add_input}
+          onChange={this.onLabelChange}
+          value={this.state.label}
         />
-        <button
-          className={styles.btn}
-          onClick={() => this.props.onItemAdded("hi")}
-        >
-          Add
-        </button>
-      </div>
+        <button className={styles.btn}>Add</button>
+      </form>
     );
   }
 }
